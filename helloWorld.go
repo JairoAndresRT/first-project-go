f93a9317af35e85b8679533e3abe1427909c42be
JairoAndresRT <jairoandresr@outlook.com>
2024-05-15 12:32:01 -0500

    Add new external package

--- helloWorld.go
+++ helloWorld.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"rsc.io/quote"
+)
 
 func main() {
 	fmt.Println("Holis mundo")
+	fmt.Println(quote.Opt())
+	fmt.Println(quote.Glass())
+	fmt.Println(quote.Go())
 }
